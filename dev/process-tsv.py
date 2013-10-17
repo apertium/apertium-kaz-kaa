5646576a56edd5beb1b6abe4d4872644b69b1b1a
Francis M. Tyers <ftyers@prompsit.com>
2013-10-17 15:46:22 +0000

    new bidix

--- dev/process-tsv.py
+++ dev/process-tsv.py
@@ -9,6 +9,28 @@ def proc_word(s): #{
 	return o;
 #}
 
+punctuation = '''
+
+    <!-- SECTION: Punctuation -->
+    
+    
+    <e><p><l>,<s n="cm"/></l><r>,<s n="cm"/></r></p></e>
+    <e><p><l>(<s n="lpar"/></l><r>(<s n="lpar"/></r></p></e>
+    <e><p><l>)<s n="rpar"/></l><r>)<s n="rpar"/></r></p></e>
+    <e><p><l>[<s n="lpar"/></l><r>[<s n="lpar"/></r></p></e>
+    <e><p><l>]<s n="rpar"/></l><r>]<s n="rpar"/></r></p></e>
+    <e><p><l>«<s n="lquot"/></l><r>«<s n="lquot"/></r></p></e>
+    <e><p><l>»<s n="rquot"/></l><r>»<s n="rquot"/></r></p></e>
+    <e><p><l>“<s n="lquot"/></l><r>“<s n="lquot"/></r></p></e>
+    <e><p><l>”<s n="rquot"/></l><r>”<s n="rquot"/></r></p></e>
+    <e><p><l>-<s n="guio"/></l><r>-<s n="guio"/></r></p></e>
+    <e><p><l>—<s n="guio"/></l><r>—<s n="guio"/></r></p></e>
+    <e><p><l>'<s n="apos"/></l><r>'<s n="apos"/></r></p></e>
+    <e><re>[.\?;:!"]+</re><p><l><s n="sent"/></l><r><s n="sent"/></r></p></e>
+    <e><re>[№%]?[0-9]+([.,][0-9]+)*[.,]*</re><p><l><s n="num"/></l><r><s n="num"/></r></p></e> 
+
+''';
+
 pos_list = ['cnjcoo', 'cnjsub', 'post', 'pron', 'det', 'num', 'n', 'adj', 'adv', 'vt', 'vi'];
 
 pos_name = {
@@ -37,6 +59,21 @@ pos_sym = {
 	'vi': '<s n="v"/><s n="iv"/>'
 };
 
+sym_list = ['post', 'cnjcoo', 'cnjsub', 'prn', 'det', 'num', 'n', 'adj', 'adv', 'v', 'tv', 'iv', 'sent', 'lpar', 'rpar', 'guio', 'cm', 'apos', 'lquot', 'rquot'];
+
+
+print('<dictionary>');
+print('  <alphabet/>');
+print('  <sdefs>');
+for sym in sym_list: #{
+	print('    <sdef n="%s"/>' % sym);
+#}
+print('  </sdefs>');
+print('  <section id="main" type="standard">');
+
+num_entries = 0;
+num_checked = 0;
+
 pos_word = {};
 
 for line in sys.stdin.readlines(): #{
@@ -50,6 +87,8 @@ for line in sys.stdin.readlines(): #{
 		continue;
 	#}
 
+	num_entries = num_entries + 1;
+
 	row = line.split('\t');
 
 	if row[3] == 'x': #{
@@ -59,15 +98,17 @@ for line in sys.stdin.readlines(): #{
 		if (row[0], row[2]) not in pos_word[row[1]]: #{
 			pos_word[row[1]].append((row[0], row[2]));
 		#}
+		num_checked = num_checked + 1;
 		#print(row[0], row[1], row[2], row[1]);
 	else: #{
 		continue;
 	#}
+
 #}
 
 for pos in pos_list: #{
 	if pos in pos_name: #{
-		print('    <!-- SECTION: ', pos_name[pos], ' -->');
+		print('    <!-- SECTION:', pos_name[pos], '-->');
 	#}
 	if pos not in pos_word: #{
 		continue;
@@ -79,3 +120,10 @@ for pos in pos_list: #{
 	#}
 	print('');
 #}
+
+print(punctuation);
+
+print('  </section>');
+print('</dictionary>');
+
+print(num_entries, num_checked, file=sys.stderr);
