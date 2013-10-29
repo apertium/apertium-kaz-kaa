3950867bfdc9edad8a39726eee463cb558bf9926
Francis M. Tyers <ftyers@prompsit.com>
2013-10-29 21:22:50 +0000

    add a script to convert ODS to TSV

---
+++ dev/ods-to-tsv.sh
@@ -0,0 +1,2 @@
+
+unzip -p $1 content.xml  | sed 's/<\/table:table-cell><table:table-cell/<\/table:table-cell>@<table:table-cell/g'  | xmllint --format - | python3 ~/scripts/strip_html.py  | sed 's/^ *//g' | sed 's/@/\t/g' | grep -v '^$' | sed 's/$/\t\t/g' | sed 's/\t\t\t\t/\t\t/g' | sed 's/\t\t\t/\t\t/g' 
