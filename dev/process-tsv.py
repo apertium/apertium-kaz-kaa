ea60498b75044491c17ffbd963bb03834deb5337
Francis M. Tyers <ftyers@prompsit.com>
2013-10-31 23:03:39 +0000

    update bidix

--- dev/process-tsv.py
+++ dev/process-tsv.py
@@ -40,6 +40,8 @@ sec_punctuation = '''
 sec_abbreviations = '''
     <!-- SECTION: Abbreviations -->
 
+    <e><p><l>ж.<s n="abbr"/></l><r>j.<s n="abbr"/></r></p></e>
+    <e><p><l>жж.<s n="abbr"/></l><r>jj.<s n="abbr"/></r></p></e>
     <e><p><l>т.б.<s n="abbr"/></l><r>t.b.<s n="abbr"/></r></p></e>
     <e><p><l>кг<s n="abbr"/></l><r>kg<s n="abbr"/></r></p></e>
     <e><p><l>км<s n="abbr"/></l><r>km<s n="abbr"/></r></p></e>
@@ -89,6 +91,36 @@ pos_static['pron'] = '''
     <e><p><l>ол<s n="prn"/><s n="pers"/></l><r>ol<s n="prn"/><s n="pers"/></r></p></e>
     <e><p><l>біз<s n="prn"/><s n="pers"/></l><r>biz<s n="prn"/><s n="pers"/></r></p></e>
     <e><p><l>сіз<s n="prn"/><s n="pers"/></l><r>siz<s n="prn"/><s n="pers"/></r></p></e>
+''';
+
+pos_static['cnjcoo'] = '''
+
+    <e><p><l>да<s n="cnjcoo"/></l><r>da<s n="cnjcoo"/></r></p></e>
+
+''';
+
+pos_static['num'] = '''
+    <e><p><l>I<s n="num"/></l><r>I<s n="num"/></r></p></e>
+    <e><p><l>II<s n="num"/></l><r>II<s n="num"/></r></p></e>
+    <e><p><l>III<s n="num"/></l><r>III<s n="num"/></r></p></e>
+    <e><p><l>IV<s n="num"/></l><r>IV<s n="num"/></r></p></e>
+    <e><p><l>V<s n="num"/></l><r>V<s n="num"/></r></p></e>
+    <e><p><l>VI<s n="num"/></l><r>VI<s n="num"/></r></p></e>
+    <e><p><l>VII<s n="num"/></l><r>VII<s n="num"/></r></p></e>
+    <e><p><l>VIII<s n="num"/></l><r>VIII<s n="num"/></r></p></e>
+    <e><p><l>IX<s n="num"/></l><r>IX<s n="num"/></r></p></e>
+    <e><p><l>X<s n="num"/></l><r>X<s n="num"/></r></p></e>
+    <e><p><l>XI<s n="num"/></l><r>XI<s n="num"/></r></p></e>
+    <e><p><l>XII<s n="num"/></l><r>XII<s n="num"/></r></p></e>
+    <e><p><l>XIII<s n="num"/></l><r>XIII<s n="num"/></r></p></e>
+    <e><p><l>XIV<s n="num"/></l><r>XIV<s n="num"/></r></p></e>
+    <e><p><l>XV<s n="num"/></l><r>XV<s n="num"/></r></p></e>
+    <e><p><l>XVI<s n="num"/></l><r>XVI<s n="num"/></r></p></e>
+    <e><p><l>XVII<s n="num"/></l><r>XVII<s n="num"/></r></p></e>
+    <e><p><l>XVIII<s n="num"/></l><r>XVIII<s n="num"/></r></p></e>
+    <e><p><l>XIX<s n="num"/></l><r>XIX<s n="num"/></r></p></e>
+    <e><p><l>XX<s n="num"/></l><r>XX<s n="num"/></r></p></e>
+    <e><p><l>XXI<s n="num"/></l><r>XXI<s n="num"/></r></p></e>
 ''';
 
 print('<dictionary>');
